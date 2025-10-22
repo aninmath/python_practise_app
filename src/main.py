a8bd7324b9d1f2dbc4451def3ed93320bab7eca0
Anindya Sarkar <sarkar.bpcl@gmail.com>
2025-10-22 14:52:07 +0530

    changed

--- src/main.py
+++ src/main.py
@@ -137,7 +137,7 @@ answer = st.text_area("Write your Python code here", height=200)
 class pytem2(BaseModel):
     Verdict: Literal["Correct", "Wrong"] = Field(description="Mention the correctness of the code")
     Efficiency: int = Field(description="Give an efficiency score out of 100 measured against the most efficient code")
-    Code: Optional[str] = Field(description="Only provide this if the verdict is Correct and efficiency is less than 100")
+    Code: Optional[str] = Field(description="write the correct code for the question")
     Why : Optional[str] = Field(description="Describe why the verdict is wrong in very SHORT and CRISP way, and only provide this if the verdict is wrong")
 
 parser_py2 = PydanticOutputParser(pydantic_object=pytem2)
@@ -172,6 +172,11 @@ if st.button('✅ Check Answer'):
         with col2:
             st.markdown("### 📊 Efficiency Score")
             st.metric(label="Efficiency", value=f"{result2.Efficiency}%")
+
+        if result2.Verdict == 'Wrong' and result2.Code:
+            if st.button('check code'):
+                st.markdown("### Find corrected Code")
+                st.code(result2.Code, language='python')
 
         if result2.Verdict == 'Correct' and result2.Code:
             st.markdown("### 💡 Suggested Better Code")
